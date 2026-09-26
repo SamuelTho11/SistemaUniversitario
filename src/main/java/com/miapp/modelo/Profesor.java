c059b085849740c15cd9d6ae8612cb10f68bad2d
SamuelTho11 <sthorrensr@unicartagena.edu.co>
2026-09-25 21:52:37 -0500

    Mejoramiento de la clase Profesor
    Se realizaron modificaciones en la clase profesor para dejarla en su mejor estado, más completo y con más información.

--- src/main/java/com/miapp/modelo/Profesor.java
+++ src/main/java/com/miapp/modelo/Profesor.java
@@ -12,20 +12,44 @@ import com.miapp.servicios.IBuscador;
  */
 public class Profesor extends Persona {
     private final double salarioBase;
+    private int horasDictadas;
+    private double tarifaPorHora;
 
     public Profesor(double salarioBase, String nombre, String apellido, int id) {
         super(nombre, apellido, id);
         this.salarioBase = salarioBase;
+        this.tarifaPorHora = 50000.0;
+        this.horasDictadas = 0;
+    }
+
+    public int getHorasDictadas() {
+        return horasDictadas;
+    }
+
+    public void setHorasDictadas(int horasDictadas) {
+        this.horasDictadas = horasDictadas;
+    }
+
+    public double getTarifaPorHora() {
+        return tarifaPorHora;
+    }
+
+    public void setTarifaPorHora(double tarifaPorHora) {
+        this.tarifaPorHora = tarifaPorHora;
     }
     
-    public void impartirClase(){
-        
+    
+    
+    public void impartirClase(int horas){
+        if(horas>0){
+            this.horasDictadas+=horas;
+            System.out.println("El profesor "+ getNombre() +" dictó "+ horas+ " horas de clase.");
+        }
     }
 
     @Override
     public double calcularPago() {
-        //salario base por las horas
-        return salarioBase;
+        return salarioBase + (horasDictadas * tarifaPorHora);
     }
 
 }
